227170413f60486beef4a55ed781876a5fbc154d
James Bury <jamesbury77@gmail.com>
2025-08-26 15:10:55 -0400

    feat: adds script to copy json files to vscode

---
+++ copy-configs.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cp settings.json $HOME/Library/Application\ Support/Code/User/settings.json
+echo "Copied settings.json to $HOME/Library/Application Support/Code/User/settings.json"
+cp keybindings.json $HOME/Library/Application\ Support/Code/User/keybindings.json
+echo "Copied keybindings.json to $HOME/Library/Application Support/Code/User/keybindings.json"
